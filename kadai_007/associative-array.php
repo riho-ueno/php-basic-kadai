a65133091289169c3fa6a457c6a0b054b8e2a022
riho-ueno <fnk1025@au.com>
2024-08-19 15:22:37 +0900

    連想配列を作って値とキーを出力しよう

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>連想配列を作って値とキーを出力しよう</title>
+</head>
+<body>
+  <?php
+    $onion  = ['name' => 'onion','price' => 200,'weight' => 160];
+    print_r($onion);
+   ?>
+</body>
+</html>
