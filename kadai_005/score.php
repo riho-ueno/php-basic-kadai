96cd6bd43d8cb2ef7f19e7905dba35c9099902fc
riho-ueno <fnk1025@au.com>
2024-08-19 15:04:03 +0900

    テストの平均点を計算しよう

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -17,19 +17,10 @@
   $score8 = 95;
   $score9 = 30;
   $score10 = 60;
-
+  echo $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+  echo '<br>';
   echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10) / 10;
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   ?>
 </body>
 </html>
